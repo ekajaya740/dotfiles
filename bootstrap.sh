20069a85876d6ebe0b1e18dc7df2252c4e2cb63d
ekajaya740 <ekajaya740@gmail.com>
2025-04-04 19:31:31 +0800

    feat: add essentials for arch-based distro

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,12 +14,17 @@ runDebian() {
 
 runUbuntu() {
   sudo apt update
-  sudo apt install stow
+  sudo apt install stow vim python3 python3-pip
   stow . -v
 }
 
 runArch() {
-  sudo pacman -Syu stow
+  sudo pacman -Syu stow vim neovim python3 docker rbenv zsh tmux dbeaver code discord gimp atac vlc wezterm
+
+  sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
+
+  sudo yay -Syu wps-office google-chrome mongodb-compass postman-bin raindrop zoom
+
   stow . -v
 }
 
