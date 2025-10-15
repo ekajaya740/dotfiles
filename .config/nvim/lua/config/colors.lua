57907c39c5fb071cfd1e402c6722d3213d7b2eb4
ekajaya <ekajaya740@gmail.com>
2025-10-15 10:48:36 +0900

    fix: change theme to synthwave and separate colors config to seemlessly enable multi theme

---
+++ .config/nvim/lua/config/colors.lua
@@ -0,0 +1,3 @@
+-- vim.cmd.colorscheme("catppuccin-mocha")
+-- vim.cmd.colorscheme("tokyonight")
+vim.cmd.colorscheme("synthwave84")
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,13 @@
 return {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-mocha",
-    },
+    "LunarVim/synthwave84.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    priority = 1000,
   },
 }
